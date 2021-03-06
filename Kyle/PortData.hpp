5d574719231e5a756b39d064757c41c59c0eecdb
swiftdesginer0 <25258597+swiftdesginer0@users.noreply.github.com>
2021-03-06 11:04:33 -0800

    Kyle's Folder Update (1)
    Created csv and updated PortData.hpp.

--- Kyle/PortData.hpp
+++ Kyle/PortData.hpp
@@ -5,6 +5,8 @@
 //  Created by Kyle Parker on 3/6/21.
 //
 
+// NOTE: 'service-names-port-numbers.csv' downloaded from https://www.iana.org/assignments/service-names-port-numbers/service-names-port-numbers.xhtml
+
 #ifndef PortData_hpp
 #define PortData_hpp
 
@@ -14,15 +16,28 @@
 using std::unordered_map;
 using std::string;
 
+class Port {
+public:
+    enum protocol: char {
+        TCP = 't',
+        UDP = 'u',
+        DCCP = 'd',
+        SCTP = 's',
+        UNKNOWN = 'k'
+    };
+    
+    string serviceName;
+    
+};
+
 class PortData {
     
     
 public:
     static unordered_map<string, int> data;
     
-    void importData(string fileName);
+    void importData(string fileName, bool isFilePath);
     
-    test
     
 };
 
