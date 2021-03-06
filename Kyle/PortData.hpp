dfd8cad6625dae9da7a1588151d326dc83f37dbc
swiftdesginer0 <25258597+swiftdesginer0@users.noreply.github.com>
2021-03-06 10:47:38 -0800

    Kyle's Folder
    First update of files.

---
+++ Kyle/PortData.hpp
@@ -0,0 +1,31 @@
+//
+//  loadPortData.hpp
+//  CrimsonCode
+//
+//  Created by Kyle Parker on 3/6/21.
+//
+
+#ifndef PortData_hpp
+#define PortData_hpp
+
+#include <iostream>
+#include <unordered_map>
+
+using std::unordered_map;
+using std::string;
+
+class PortData {
+    
+    
+public:
+    static unordered_map<string, int> data;
+    
+    void importData(string fileName);
+    
+    test
+    
+};
+
+
+
+#endif /* loadPortData_hpp */
---
+++ Kyle/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  CrimsonCode
+//
+//  Created by Kyle Parker on 3/6/21.
+//
+
+#include "PortData.hpp"
+
+int main(int argc, const char * argv[]) {
+    
+    
+    
+    return 0;
+}
